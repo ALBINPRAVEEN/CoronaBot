e7391d93c23a511f2e7c445855bf348bc944641a
ALBY <68139513+ALBINPRAVEEN@users.noreply.github.com>
2021-12-15 08:58:23 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# Corona Info Bot
+A corona information telegram bot
+
+---
+
+## Variables
+
+- `API_HASH` Your API Hash from my.telegram.org
+- `API_ID` Your API ID from my.telegram.org
+- `BOT_TOKEN` Your bot token from @BotFather
+
+---
---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+import requests
+from requests.utils import requote_uri
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+Bot = Client(
+    "Corona-Info-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+API = "https://api.sumanjay.cf/covid/?country="
+
+START_TEXT = """Hello {},
+I am a simple corona information of a country telegram bot.
+
+Made by @i_am_albin_praveen"""
+
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join Updates Channel ⚙', url='https://telegram.me/musicwithalby')]])
+
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=BUTTONS,
+        quote=True
+    )
+
+
+@Bot.on_message(filters.private & filters.text)
+async def reply_info(bot, update):
+    reply_markup = BUTTONS
+    await update.reply_text(
+        text=covid_info(update.text),
+        disable_web_page_preview=True,
+        quote=True,
+        reply_markup=reply_markup
+    )
+
+
+def covid_info(country_name):
+    try:
+        r = requests.get(API + requote_uri(country_name.lower()))
+        info = r.json()
+        country = info['country'].capitalize()
+        active = info['active']
+        confirmed = info['confirmed']
+        deaths = info['deaths']
+        info_id = info['id']
+        last_update = info['last_update']
+        latitude = info['latitude']
+        longitude = info['longitude']
+        recovered = info['recovered']
+        covid_info = f"""--**Covid 19 Information**--
+
+Country : `{country}`
+Actived : `{active}`
+Confirmed : `{confirmed}`
+Deaths : `{deaths}`
+ID : `{info_id}`
+Last Update : `{last_update}`
+Latitude : `{latitude}`
+Longitude : `{longitude}`
+Recovered : `{recovered}`
+
+Made by @i_am_albin_praveen"""
+        return covid_info
+    except Exception as error:
+        return error
+
+
+Bot.run()
